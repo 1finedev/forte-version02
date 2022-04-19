f4df241e287f2689aa444f5bc50fbb6e98d2db81
Klin-Coders <49831574+Klin-Coders@users.noreply.github.com>
2022-04-19 15:05:15 +0300

    fix

--- pages/api/shipments/addBulk.js
+++ pages/api/shipments/addBulk.js
@@ -1,55 +1,54 @@
-import Shipment from "./../../../backend/shipmentModel";
-import all from "./../../../Mock.json";
-const handler = async (req, res) => {
-  try {
-    // const ship = await Shipment.find({}).limit(231);
+// import Shipment from "./../../../backend/shipmentModel";
+// const handler = async (req, res) => {
+//   try {
+//     // const ship = await Shipment.find({}).limit(231);
 
-    // console.log(ship[0]);
+//     // console.log(ship[0]);
 
-    // ship.forEach(async (doc) => {
-    //   const del = await Shipment.findByIdAndDelete(doc._id);
-    //   console.log(del);
-    // });
+//     // ship.forEach(async (doc) => {
+//     //   const del = await Shipment.findByIdAndDelete(doc._id);
+//     //   console.log(del);
+//     // });
 
-    // console.log(all.length);
-    all.map(async (doc) => {
-      const create = await Shipment.create({
-        name: doc.name.toUpperCase(),
-        destination: doc.destination.toUpperCase(),
-        weight: doc.weight,
-        carton: doc.carton,
-        mobile: doc.mobile,
-        shipCode: doc.shipCode,
-        shipStatus:
-          doc.shipstatus === "WAREHOUSE [LAGOS-NIGERIA]"
-            ? "WAREHOUSE"
-            : doc.shipstatus,
-        action: doc.action,
-        shipType: doc.shipType,
-        paymentStatus: doc.paymentStatus,
-        createdAt: doc.createdAt,
-        dollarRate: doc.dollar_rate,
-        customsRate: doc.customs_rate,
-        freightRate: doc.freightRate,
-        freightInDollars: doc.freight_$,
-        freightTotal: doc.freight,
-        customsTotal: doc.customs,
-        amountDue: doc.amountDue,
-        shipMode: doc.shipMode,
-        locked: true,
-        messageSent: true,
-        calculated: true,
-        user: doc.user._id,
-      });
-      console.log(create);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+//     // console.log(all.length);
+//     // all.map(async (doc) => {
+//     //   const create = await Shipment.create({
+//     //     name: doc.name.toUpperCase(),
+//     //     destination: doc.destination.toUpperCase(),
+//     //     weight: doc.weight,
+//     //     carton: doc.carton,
+//     //     mobile: doc.mobile,
+//     //     shipCode: doc.shipCode,
+//     //     shipStatus:
+//     //       doc.shipstatus === "WAREHOUSE [LAGOS-NIGERIA]"
+//     //         ? "WAREHOUSE"
+//     //         : doc.shipstatus,
+//     //     action: doc.action,
+//     //     shipType: doc.shipType,
+//     //     paymentStatus: doc.paymentStatus,
+//     //     createdAt: doc.createdAt,
+//     //     dollarRate: doc.dollar_rate,
+//     //     customsRate: doc.customs_rate,
+//     //     freightRate: doc.freightRate,
+//     //     freightInDollars: doc.freight_$,
+//     //     freightTotal: doc.freight,
+//     //     customsTotal: doc.customs,
+//     //     amountDue: doc.amountDue,
+//     //     shipMode: doc.shipMode,
+//     //     locked: true,
+//     //     messageSent: true,
+//     //     calculated: true,
+//     //     user: doc.user._id,
+//     //   });
+//       console.log(create);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
 
-  res.status(200).json({
-    status: "success",
-  });
-};
+//   res.status(200).json({
+//     status: "success",
+//   });
+// };
 
-export default handler;
+// export default handler;
